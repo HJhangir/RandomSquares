5f87dda395f9960c33eeb25d8ddd34e7c29a943d
unknown <chuseynov538@gmail.com>
2022-09-20 21:21:11 +0400

    Random-Square

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,20 @@
-(function makeDiv(){
-    const divsize = ((Math.random()*100) + 50).toFixed();
-    const color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
-    $newdiv = $('#randBlock').css({
-    'width':divsize+'px',
-    'height':divsize+'px',
-    'background-color': color
-    });
-    
-    const posx = (Math.random() * ($(document).width() - divsize)).toFixed();
-    const posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-    
-    $newdiv.css({
-    'position':'absolute',
-    'left':posx+'px',
-    'top':posy+'px',
-    'display':'none'
-    }).appendTo( 'body' ).fadeIn(100).delay(300).fadeOut(200, function(){
-    makeDiv();
-    });
-    })();
+const MyButton = document.querySelector(`.btn`)
 
+MyButton.addEventListener(`click`, generate)
+
+
+function generate(e){
+    const div = document.createElement('div')
+
+div.style.width = "30px";
+div.style.height = "30px";
+div.style.background = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+div.style.position = "absolute";
+div.style.left = Math.random() * 1450+"px";
+div.style.top = Math.random() * 620+"px";
+
+
+document.getElementById('main').appendChild(div)
+
+
+}
